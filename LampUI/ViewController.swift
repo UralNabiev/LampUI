d88418fcd6f04fb1aede8e3007b9002af1f1beb2
UralNabiev <devuralnabiev@gmail.com>
2024-02-27 15:35:09 +0500

    Initial Commit

---
+++ LampUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LampUI
+//
+//  Created by O'ral Nabiyev on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
